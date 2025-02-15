7d4700dea46fb8ef5470f4c12ba54a00eaf78f4a
kaushik-654321 <kaushiks662@gmail.com>
2025-02-15 17:31:34 +0000

    apiroute

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,11 @@
 const API_URL = "https://fantastic-disco-x457747qvww2p5vp-5000.app.github.dev/fruits/";
 
-export const fetchData = async () => {
+export const fetchData = async (limit, page, searchQuery) => {
     try {
-        const response = await fetch(`${API_URL}/`, { credentials: "include" });
-
+        const response = await fetch(`${API_URL}?page=${page}&limit=${limit}&search=${searchQuery}`, { credentials: "include" });
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
         const data = await response.json();
         return data;
     } catch (error) {
