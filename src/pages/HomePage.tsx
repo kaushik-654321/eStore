3105bac4a6e23a808f604be123b7694814bfd0d8
kaushik-654321 <75725882+kaushik-654321@users.noreply.github.com>
2025-08-06 17:48:02 +0000

    frontend changes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,36 +10,38 @@ import Facts from './Facts';
 import Testimonials from '../testimonials';
 import { API_ENDPOINTS } from '../api/apiEndpoints';
 import { setUser } from '../features/userSlice';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../app/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../app/store';
 
 
 const Home: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
-
+  const user = useSelector((state: RootState) => state.user);
   useEffect(() => {
-    fetch(`${API_ENDPOINTS.USER.user}`, {
-      method: 'GET',
-      credentials: 'include', // 👈 Important to send session cookie
-    })
-      .then((res) => {
-        if (!res.ok) throw new Error('Not logged in');
-        return res.json();
+    if (!user?.userId) {
+      fetch(`${API_ENDPOINTS.USER.user}`, {
+        method: 'GET',
+        credentials: 'include', // 👈 Important to send session cookie
       })
-      .then((data) => {
-        const name = data?._json.name;
-        const email = data?._json.email;
-        const userId = data?.id;
-        const token = data?.id
+        .then((res) => {
+          if (!res.ok) throw new Error('Not logged in');
+          return res.json();
+        })
+        .then((data) => {
+          const name = data?._json.name;
+          const email = data?._json.email;
+          const userId = data?.id;
+          const token = data?.id
 
-        dispatch(setUser({ name, email, userId, token }));
-        console.log('✅ Logged in user:', data);
-        // setUser(userObj);
-      })
-      .catch((err) => {
-        console.log('❌ Not logged in');
-      });
-  }, [])
+          dispatch(setUser({ name, email, userId, token }));
+          console.log('✅ Logged in user:', data);
+          // setUser(userObj);
+        })
+        .catch((err) => {
+          console.log('❌ Not logged in');
+        });
+    }
+  }, [user])
 
   return (
     <>
