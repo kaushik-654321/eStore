3838010db8a66b8629b940d25f3822e9e315f2c1
kaushik-654321 <75725882+kaushik-654321@users.noreply.github.com>
2025-08-08 16:39:41 +0000

    route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,7 +7,9 @@ const userRoutes = express.Router();
 userRoutes.post('/auth/login', loginusers);
 userRoutes.post('/auth/signup', registeredUser);
 // Get logged-in user
-userRoutes.get('/user', OauthUserLoggedIn);
+userRoutes.get('/user', (req, res)=>{
+  console.log("User route hit", req.user);
+});
 // Logout Route
 userRoutes.get('/auth/logout', OauthUserLoggedOut);
 
